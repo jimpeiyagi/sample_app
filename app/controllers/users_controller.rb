16f71948c51a2da25ea71aabbc9b8fb32b782693
八木 仁平 <jimpei.yagi@bizreach.co.jp>
2015-06-07 11:27:54 +0900

    Initialize repository

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:edit, :update, :destroy, :following, :followers]
-before_action :correct_user,   only: [:edit, :update]
-before_action :admin_user,     only: :destroy
+  before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -13,17 +14,29 @@ before_action :admin_user,     only: :destroy
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -33,7 +46,7 @@ before_action :admin_user,     only: :destroy
     redirect_to users_url
   end
 
-    def following
+  def following
     @title = "Following"
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
@@ -54,29 +67,14 @@ before_action :admin_user,     only: :destroy
                                    :password_confirmation)
     end
 
-
-
-    # Before actions
-
+    # Before filters
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-
-    def edit
-    end
-
-    def update
-      if @user.update_attributes(user_params)
-        flash[:success] = "Profile updated"
-        redirect_to @user
-      else
-        render 'edit'
-      end
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
-end
+  end
