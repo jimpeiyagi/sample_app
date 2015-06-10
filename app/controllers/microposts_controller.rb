cbcf5151596150231a2ec2001658072f5f26284d
八木 仁平 <jimpei.yagi@bizreach.co.jp>
2015-06-10 19:28:12 +0900

    iroiro

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -21,7 +21,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :ridedate, :from, :to, :can_ride_person, :ride_on_flg)
     end
 
     def correct_user
