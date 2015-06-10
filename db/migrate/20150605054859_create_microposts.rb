4f75d261d1e997f6d987a3bbee3bf39f6b10efc3
八木 仁平 <jimpei.yagi@bizreach.co.jp>
2015-06-10 10:16:57 +0900

    DBにカラムを追加

--- db/migrate/20150605054859_create_microposts.rb
+++ db/migrate/20150605054859_create_microposts.rb
@@ -3,6 +3,11 @@ class CreateMicroposts < ActiveRecord::Migration
     create_table :microposts do |t|
       t.string :content
       t.integer :user_id
+      t.datetime :ridedate
+      t.string :from
+      t.string :to
+      t.integer :can_ride_person
+      t.boolean :ride_on_flg
 
       t.timestamps
     end
