402809de43e0b6dcf54c1e7353eaf100111ddb9b
八木 仁平 <jimpei.yagi@bizreach.co.jp>
2015-06-11 17:51:41 +0900

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610013533) do
+ActiveRecord::Schema.define(version: 20150605083414) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.datetime "ridedate"
     t.string   "from"
     t.string   "to"
     t.integer  "can_ride_person"
     t.boolean  "ride_on_flg"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20150610013533) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin"
-    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
